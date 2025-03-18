4235e3f8de8a49a8333853242fb784afc46a3655
Antoine GREUZARD <antoine.greuzard@cgi.com>
2025-03-18 11:42:13 +0100

    fix: wrong App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,19 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125"/>
+  <div class="flex flex-col min-h-screen">
+    <!-- Navbar -->
+    <Navbar/>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!"/>
+    <!-- Contenu principal -->
+    <main class="flex-1 container mx-auto p-4">
+      <router-view/>
+    </main>
 
-      <nav>
-        <RouterLink to="/">Accueil</RouterLink> |
-        <RouterLink to="/about">À propos</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView/>
+    <!-- Footer -->
+    <Footer/>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<script setup lang="ts">
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
+</script>
