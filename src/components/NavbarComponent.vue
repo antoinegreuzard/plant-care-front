1c77502bf50e4478d16c41cef3533f9e7f8905ff
Antoine GREUZARD <antoine.greuzard@cgi.com>
2025-03-18 12:25:59 +0100

    fix: add button add

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -2,11 +2,26 @@
   <nav class="bg-green-600 text-white p-4">
     <div class="container mx-auto flex justify-between">
       <router-link to="/" class="text-lg font-bold">🌱 Plant Care</router-link>
-      <div>
-        <router-link v-if="!isAuthenticated" to="/login" class="px-4 py-2 bg-white text-green-600 rounded">
+      <div class="flex space-x-4">
+        <router-link
+          v-if="isAuthenticated"
+          to="/add-plant"
+          class="px-4 py-2 bg-white text-green-600 rounded"
+        >
+          Ajouter une plante
+        </router-link>
+        <router-link
+          v-if="!isAuthenticated"
+          to="/login"
+          class="px-4 py-2 bg-white text-green-600 rounded"
+        >
           Connexion
         </router-link>
-        <button v-else @click="logout" class="px-4 py-2 bg-red-600 text-white rounded">
+        <button
+          v-else
+          @click="logout"
+          class="px-4 py-2 bg-red-600 text-white rounded"
+        >
           Déconnexion
         </button>
       </div>
