316ed9d6c322e6ec33f3cada558b45227fc70aa0
Antoine GREUZARD <antoine.greuzard@cgi.com>
2025-03-18 12:24:05 +0100

    fix: isAuthenticated

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,17 +1,18 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    isAuthenticated: !!localStorage.getItem('jwt')
-  }),
-  actions: {
-    login(token: string) {
-      localStorage.setItem('jwt', token)
-      this.isAuthenticated = true
-    },
-    logout() {
-      localStorage.removeItem('jwt')
-      this.isAuthenticated = false
-    }
+export const useAuthStore = defineStore('auth', () => {
+  const isAuthenticated = ref(!!localStorage.getItem('jwt'))
+
+  function login(token: string) {
+    localStorage.setItem('jwt', token)
+    isAuthenticated.value = true
   }
+
+  function logout() {
+    localStorage.removeItem('jwt')
+    isAuthenticated.value = false
+  }
+
+  return { isAuthenticated, login, logout }
 })
