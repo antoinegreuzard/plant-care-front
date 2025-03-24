c659f22e9063c0134396805f30c98e451282e634
Antoine GREUZARD <antoin.greuzard@gmail.com>
2025-03-24 18:57:52 +0100

    fix: use 127.0.0.1

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,31 +1,17 @@
 Cypress.Commands.add('login', (username, password) => {
-  const tryLogin = (url) => {
-    return cy.request({
-      method: 'POST',
-      url: `${url}/api/token/`,
-      body: { username, password },
-      failOnStatusCode: false
-    })
-  }
-
-  tryLogin('http://localhost:8000').then((response) => {
-    if (response.status === 200) {
-      const token = response.body.access
-      localStorage.setItem('jwt', token)
-      cy.setCookie('jwt', token)
-      cy.visit('/')
-    } else {
-      // Si localhost échoue, on tente avec 127.0.0.1
-      tryLogin('http://127.0.0.1:8000').then((response2) => {
-        if (response2.status !== 200) {
-          throw new Error(`Échec de l'authentification : ${response2.body.detail}`)
-        }
-
-        const token = response2.body.access
-        localStorage.setItem('jwt', token)
-        cy.setCookie('jwt', token)
-        cy.visit('/')
-      })
+  cy.request({
+    method: 'POST',
+    url: 'http://127.0.0.1:8000/api/token/',
+    body: { username, password },
+    failOnStatusCode: false // Empêche Cypress de planter en cas d'erreur
+  }).then((response) => {
+    if (response.status !== 200) {
+      throw new Error(`Échec de l'authentification : ${response.body.detail}`)
     }
+
+    const token = response.body.access
+    localStorage.setItem('jwt', token)
+    cy.setCookie('jwt', token)
+    cy.visit('/')
   })
 })
